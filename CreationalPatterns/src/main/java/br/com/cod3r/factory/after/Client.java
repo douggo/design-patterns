60a27b9afdca5c8cf8412d57818f08437596034b
Douglas Felipe da Silva <dev.douggo@gmail.com>
2022-09-22 22:40:18 -0300

    Inicia o desenvolvimento do Factory Method

--- CreationalPatterns/src/main/java/br/com/cod3r/factory/after/Client.java
+++ CreationalPatterns/src/main/java/br/com/cod3r/factory/after/Client.java
@@ -1,4 +1,4 @@
-package br.com.cod3r.factory.apple;
+package br.com.cod3r.factory.after;
 
 public class Client {
 	
---
+++ CreationalPatterns/src/main/java/br/com/cod3r/factory/after/factory/IPhoneFactory.java
@@ -0,0 +1,6 @@
+package br.com.cod3r.factory.after.factory;
+
+public abstract class IPhoneFactory {
+
+	
+}
