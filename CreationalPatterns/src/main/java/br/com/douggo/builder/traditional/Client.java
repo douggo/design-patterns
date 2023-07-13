a1d21e764da0755183ec42a7ffb22a017c080f8f
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-07-13 08:24:22 -0300

    Using the GOF method

--- CreationalPatterns/src/main/java/br/com/douggo/builder/traditional/Client.java
+++ CreationalPatterns/src/main/java/br/com/douggo/builder/traditional/Client.java
@@ -1,8 +1,16 @@
 package br.com.douggo.builder.traditional;
 
-public class Client {
-	
-	public static void main(String[] args) {
+import br.com.cod3r_base.builder.meal.model.FastFoodMeal;
+import br.com.douggo.builder.traditional.builder.FullMealBuilder;
+import br.com.douggo.builder.traditional.director.MealDirector;
 
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("Creating a full meal course:");
+		FastFoodMeal meal = new FastFoodMeal();
+		MealDirector director = new MealDirector(new FullMealBuilder(meal));
+		FastFoodMeal combo = director.createCombo();
+		System.out.println(combo);
 	}
 }
--- CreationalPatterns/src/main/java/br/com/douggo/builder/traditional/director/MealDirector.java
+++ CreationalPatterns/src/main/java/br/com/douggo/builder/traditional/director/MealDirector.java
@@ -1,5 +1,23 @@
 package br.com.douggo.builder.traditional.director;
 
+import br.com.cod3r_base.builder.meal.model.FastFoodMeal;
+import br.com.douggo.builder.traditional.builder.FastFoodMealBuilder;
+
 public class MealDirector {
+
+    private final FastFoodMealBuilder builder;
+
+    public MealDirector(FastFoodMealBuilder builder) {
+        this.builder = builder;
+    }
+
+    public FastFoodMeal createCombo() {
+        this.builder.buildMain();
+        this.builder.buildSide();
+        this.builder.buildDrink();
+        this.builder.buildDesert();
+        this.builder.buildGift();
+        return this.builder.getMeal();
+    }
 
 }
