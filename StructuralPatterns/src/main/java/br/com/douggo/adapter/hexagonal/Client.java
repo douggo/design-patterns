6c14db81f51200785fcb5d66f3e6e66cd7e7cb35
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-12 15:44:52 -0300

    Implementing adapters using Hexagonal Arch

--- StructuralPatterns/src/main/java/br/com/douggo/adapter/hexagonal/Client.java
+++ StructuralPatterns/src/main/java/br/com/douggo/adapter/hexagonal/Client.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class Client {
 
 	public static void main(String[] args) {
+		System.out.println("------ Inserindo usuário ------");
 		UserRest userRestApi = new UserRest();
 		Map<String, String> body = new HashMap<String, String>();
 		body.put("email", "user1@email.com");
@@ -15,24 +16,29 @@ public class Client {
 		body.put("password", "passwd");
 		Integer statusCode = userRestApi.post(body);
 		System.out.println(statusCode);
-		
-		statusCode = userRestApi.get();
-		System.out.println(statusCode);
-		
+
+		System.out.println();
+
+		System.out.println("------ Inserindo usuários ------");
 		body.put("email", "user2@email.com");
 		body.put("name", "User 2");
 		statusCode = userRestApi.post(body);
-		System.out.println(statusCode);
-		
-		statusCode = userRestApi.get();
-		System.out.println(statusCode);
-		
+		System.out.println("Usuario 1: " +statusCode);
 		body.put("email", "user2@email.com");
 		body.put("name", "User 2");
 		statusCode = userRestApi.post(body);
-		System.out.println(statusCode);
-		
+		System.out.println("Usuario 2: " +statusCode);
+
+		System.out.println();
+
+		System.out.println("------ Retornando todos os usuários ------");
 		statusCode = userRestApi.get();
+		System.out.println(statusCode);
+
+		System.out.println();
+
+		System.out.println("------ Buscando usuário user1@gmail.com ------");
+		statusCode = userRestApi.getUser("user1@email.com");
 		System.out.println(statusCode);
 	}
 }
