4eef3ad2a2ad8d087c63a0b0815e5f5dbbfacffd
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-12 13:43:44 -0300

    Implements adapter

--- StructuralPatterns/src/main/java/br/com/douggo/adapter/tvPort/adapters/HDMIToVGAAdapter.java
+++ StructuralPatterns/src/main/java/br/com/douggo/adapter/tvPort/adapters/HDMIToVGAAdapter.java
@@ -1,5 +1,25 @@
 package br.com.douggo.adapter.tvPort.adapters;
 
-public class HDMIToVGAAdapter {
+import br.com.douggo.adapter.tvPort.intefaces.HDMI;
+import br.com.douggo.adapter.tvPort.intefaces.VGA;
 
+public class HDMIToVGAAdapter implements HDMI {
+
+    private final VGA monitor;
+
+    public HDMIToVGAAdapter(VGA monitor) {
+        System.out.println("Connecting adapter to the VGA Monitor");
+        this.monitor = monitor;
+    }
+
+    @Override
+    public void setImage(String image) {
+        System.out.println("Converting image from HDMI to VGA...");
+        this.monitor.setImage(image);
+    }
+
+    @Override
+    public void setSound(String sound) {
+        System.out.println("Sorry, VGA can't process sound");
+    }
 }
