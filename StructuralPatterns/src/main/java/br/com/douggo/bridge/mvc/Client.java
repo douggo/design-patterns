d337d6acbe4ac35d7526ff823baa262554517950
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-12 20:25:22 -0300

    Initialize before state

---
+++ StructuralPatterns/src/main/java/br/com/douggo/bridge/mvc/Client.java
@@ -0,0 +1,13 @@
+package br.com.douggo.bridge.mvc;
+
+import br.com.douggo.bridge.mvc.model.User;
+
+public class Client {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		User user = new User("user", "user@email.com", "passwd");
+		
+		
+	}
+}
