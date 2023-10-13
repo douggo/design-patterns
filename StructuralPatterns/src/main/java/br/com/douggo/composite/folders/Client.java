4c6a46aadd4a4d2f85e69d18d438f2807cba5162
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-13 07:57:32 -0300

    Initialize with before state

---
+++ StructuralPatterns/src/main/java/br/com/douggo/composite/folders/Client.java
@@ -0,0 +1,8 @@
+package br.com.douggo.composite.folders;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ StructuralPatterns/src/main/java/br/com/douggo/composite/folders/model/Component.java
@@ -0,0 +1,5 @@
+package br.com.douggo.composite.folders.model;
+
+public interface Component {
+	
+}
