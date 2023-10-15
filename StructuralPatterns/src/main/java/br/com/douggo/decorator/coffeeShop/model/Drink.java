93588c676aeb53b1eb713ddf8176baf5f56619b7
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-15 02:11:17 -0300

    Initialize with before state

---
+++ StructuralPatterns/src/main/java/br/com/douggo/decorator/coffeeShop/model/Drink.java
@@ -0,0 +1,6 @@
+package br.com.douggo.decorator.coffeeShop.model;
+
+public interface Drink {
+	void serve();
+	Double getPrice();
+}
---
+++ StructuralPatterns/src/main/java/br/com/douggo/decorator/coffeeShop/model/decorators/DrinkDecorator.java
@@ -0,0 +1,7 @@
+package br.com.douggo.decorator.coffeeShop.model.decorators;
+
+import br.com.douggo.decorator.coffeeShop.model.Drink;
+
+public abstract class DrinkDecorator implements Drink {
+	
+}
