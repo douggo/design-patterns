7c742ab40e4715cbe4498a7bf9a2bf1ad56e4adc
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-16 07:24:12 -0300

    Implements decorator using Swing API

--- StructuralPatterns/src/main/java/br/com/douggo/decorator/swing/Client.java
+++ StructuralPatterns/src/main/java/br/com/douggo/decorator/swing/Client.java
@@ -6,14 +6,18 @@ import java.awt.*;
 public class Client {
 
 	public static void main(String[] args) {
-		JFrame frame =new JFrame();  
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame frame =new JFrame();
 		frame.setLayout(new FlowLayout());
-		frame.setSize(400,200);  
-		
-		JTextArea textArea = new JTextArea(10, 30);
-		frame.add(textArea);
-		          
+
+		JScrollPane scroll = new JScrollPane(
+				new JTextArea(10, 30),
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS
+		);
+		frame.add(scroll);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,200);
 		frame.setVisible(true);
 	}
 }
