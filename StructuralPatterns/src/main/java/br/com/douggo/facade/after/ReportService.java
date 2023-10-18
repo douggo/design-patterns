9a3812e3715938d7c9d181695fa9e6677d34a04c
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-17 23:21:59 -0300

    Initialize with before state, to implement facade

---
+++ StructuralPatterns/src/main/java/br/com/douggo/facade/after/ReportService.java
@@ -0,0 +1,23 @@
+package br.com.douggo.facade.after;
+
+import java.util.List;
+
+public class ReportService {
+	private RegisterService registerService;
+	
+	public ReportService(RegisterService registerService) {
+		this.registerService = registerService;
+	}
+
+	public void getSumary(Card card) {
+		List<Register> registers = registerService.getRegistersByCard(card);
+		for(Register reg: registers) {
+			System.out.println(String.format("%s\t%.2f\t%s", 
+					reg.getStoreName(),
+					reg.getValue(),
+					reg.getDate().toString()
+					));
+		}
+	}
+
+}
