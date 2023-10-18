d1a1078f840e272b251b0602fe56d7d6de49e516
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-17 23:36:31 -0300

    Implements facade pattern

--- StructuralPatterns/src/main/java/br/com/douggo/facade/after/Client.java
+++ StructuralPatterns/src/main/java/br/com/douggo/facade/after/Client.java
@@ -1,33 +1,16 @@
 package br.com.douggo.facade.after;
 
+import br.com.douggo.facade.after.facades.CallCenterFacade;
 import br.com.douggo.facade.after.model.Card;
-import br.com.douggo.facade.after.model.Register;
-import br.com.douggo.facade.after.services.*;
-
-import java.util.List;
 
 public class Client {
 
 	public static void main(String[] args) {
-		CardService cardService = new CardService();
-		RegisterService registerService = new RegisterService();
-		ReportService reportService = new ReportService(registerService);
-		PaymentService paymentService = new PaymentService(registerService);
-		SecurityService securityService = new SecurityService(cardService, registerService);
-		
-		Card card = cardService.getCardByUser(123456L);
+		CallCenterFacade facade = new CallCenterFacade();
+		Card card = facade.getCardByUser(123456L);
 		System.out.println(card);
-		
-		reportService.getSumary(card);
-		
-		paymentService.getPaymentInfoByCard(card);
-		
-		//Remove last register, block the card e order a new one
-		List<Register> registers = registerService.getRegistersByCard(card);
-		registerService.removeByIndex(card, registers.size() - 1);
-		List<Register> pendingRegisters = securityService.blockCard(card);
-		Card newCard = cardService.createNewCard(123456L, 33445566L);
-		registerService.addCardRegisters(newCard, pendingRegisters);
-		reportService.getSumary(newCard);
+		facade.getSummary(card);
+		facade.getPaymentInfoByCard(card);
+		facade.orderNewCard(card);
 	}
 }
