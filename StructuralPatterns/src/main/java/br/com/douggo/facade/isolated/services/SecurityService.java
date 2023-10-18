d80b57fdafea05998d814d2b7d7982ae452d4a21
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-10-17 23:42:10 -0300

    Initialize with before state

---
+++ StructuralPatterns/src/main/java/br/com/douggo/facade/isolated/services/SecurityService.java
@@ -0,0 +1,24 @@
+package br.com.douggo.facade.isolated.services;
+
+import br.com.douggo.facade.isolated.model.Card;
+import br.com.douggo.facade.isolated.model.Register;
+
+import java.util.List;
+
+public class SecurityService {
+	private CardService cardService;
+	private RegisterService registerService;
+	
+	public SecurityService(CardService cardService, RegisterService registerService) {
+		this.cardService = cardService;
+		this.registerService = registerService;
+	}
+
+	public List<Register> blockCard(Card card) {
+		System.out.println("Blocking card:" + card);
+		List<Register> pendingRegistries = registerService.getRegistersByCard(card);
+		cardService.removeCard(card);
+		registerService.deleteCardRegistries(card);
+		return pendingRegistries;
+	}
+}
