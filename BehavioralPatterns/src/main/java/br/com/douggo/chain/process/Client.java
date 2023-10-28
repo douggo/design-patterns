628d55360c1c54d49673050c54812a25e8b706bd
Douglas Felipe da Silva <douggo@MacBook-Air-de-Douglas.local>
2023-10-28 19:50:43 -0300

    Implements chain of responsibility on a real case project

--- BehavioralPatterns/src/main/java/br/com/douggo/chain/process/Client.java
+++ BehavioralPatterns/src/main/java/br/com/douggo/chain/process/Client.java
@@ -1,8 +1,25 @@
 package br.com.douggo.chain.process;
 
+import br.com.douggo.chain.process.service.GenericService;
+import br.com.douggo.chain.process.service.ProcessContext;
+import br.com.douggo.chain.process.service.ServicesCatalog;
+
 public class Client {
 
 	public static void main(String[] args) {
-		
+		ProcessContext context = new ProcessContext();
+		context.put("email", "douglas.silva@gmail.com");
+		context.put("password", "1234");
+		Object response = GenericService.run(ServicesCatalog.saveUserProcess, context);
+		System.out.println(response);
+
+		context.reset();
+
+		context.put("email", "douglas.silva@gmail.com");
+		context.put("password", "1234");
+		context.put("name", "Douglas da Silva");
+
+		response = GenericService.run(ServicesCatalog.saveUserProcess, context);
+		System.out.println(response);
 	}
 }
--- BehavioralPatterns/src/main/java/br/com/douggo/chain/process/steps/SaveUser.java
+++ BehavioralPatterns/src/main/java/br/com/douggo/chain/process/steps/SaveUser.java
@@ -3,6 +3,7 @@ package br.com.douggo.chain.process.steps;
 import br.com.douggo.chain.process.repository.UserRepository;
 import br.com.douggo.chain.process.service.ProcessContext;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class SaveUser extends ProcessStep {
@@ -14,7 +15,12 @@ public class SaveUser extends ProcessStep {
 	@Override
 	@SuppressWarnings("unchecked")
 	public ProcessContext execute(ProcessContext context) throws Exception {
-		Map<String, Object> userInput = (Map<String, Object>) context.get("input");
+		Map<String, Object> userInput = new LinkedHashMap<>();
+
+		userInput.put("email", context.get("email"));
+		userInput.put("password", context.get("password"));
+		userInput.put("name", context.get("name"));
+
 		UserRepository.getInstance().saveUser(userInput);
 		return next(context, String.format("User '%s' saved!", userInput.get("name")));
 	}
