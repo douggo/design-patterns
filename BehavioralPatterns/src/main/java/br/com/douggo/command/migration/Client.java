e475c335330faef55fb89b9595f6d88ea0ecc5c2
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-11-01 07:51:57 -0300

    Creates a situation where the migrations are run and removed

--- BehavioralPatterns/src/main/java/br/com/douggo/command/migration/Client.java
+++ BehavioralPatterns/src/main/java/br/com/douggo/command/migration/Client.java
@@ -1,8 +1,43 @@
 package br.com.douggo.command.migration;
 
+import br.com.douggo.command.migration.commands.CreateProductTableCommand;
+import br.com.douggo.command.migration.commands.CreateUserTableCommand;
+import br.com.douggo.command.migration.commands.CreateUserTableRoleColumnCommand;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Client {
 
 	public static void main(String[] args) {
+		Map<String, List<String>> database = new LinkedHashMap<>();
 
+		MigrationTool tool = new MigrationTool();
+		tool.addMigration(new CreateProductTableCommand(database));
+		tool.addMigration(new CreateUserTableCommand(database));
+		tool.addMigration(new CreateUserTableRoleColumnCommand(database));
+
+		System.out.println("Database before running migrations: " + database);
+		System.out.println("Running migrations...");
+		tool.up();
+		System.out.println(database);
+		tool.up();
+		System.out.println(database);
+		tool.up();
+		System.out.println(database);
+		tool.up();
+
+		System.out.println();
+
+		System.out.println("Current state of database: " + database);
+		System.out.println("Undoing all migrations");
+		tool.down();
+		System.out.println(database);
+		tool.down();
+		System.out.println(database);
+		tool.down();
+		System.out.println(database);
+		tool.down();
 	}
 }
