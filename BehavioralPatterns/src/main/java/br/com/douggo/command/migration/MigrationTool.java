ef3d4e75451b032277cdb38e914fbf50a9bb7f51
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-11-01 07:44:14 -0300

    Create logic to run migrations

--- BehavioralPatterns/src/main/java/br/com/douggo/command/migration/MigrationTool.java
+++ BehavioralPatterns/src/main/java/br/com/douggo/command/migration/MigrationTool.java
@@ -1,4 +1,41 @@
 package br.com.douggo.command.migration;
 
+import br.com.douggo.command.migration.commands.MigrationCommand;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class MigrationTool {
+
+    private final List<MigrationCommand> migrations;
+    private int migrationIndex;
+
+    public MigrationTool() {
+        this.migrations = new ArrayList<>();
+        this.migrationIndex = 0;
+    }
+
+    public void addMigration(MigrationCommand migration) {
+        this.migrations.add(migration);
+    }
+
+    public void up() {
+        if (this.migrations.size() > this.migrationIndex) {
+            this.migrations.get(this.migrationIndex).execute();
+            this.migrationIndex++;
+        } else {
+            System.out.println("You are up to date!");
+        }
+    }
+
+    public void down() {
+        if (this.migrationIndex > 0) {
+            this.migrations.get(this.migrationIndex - 1).undo();
+            this.migrationIndex--;
+        } else {
+            System.out.println("There are no migrations left to undo");
+        }
+    }
+
 }
