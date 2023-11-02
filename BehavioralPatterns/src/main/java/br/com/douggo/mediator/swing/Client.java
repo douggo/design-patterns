21acf3d9f43d3f5d39670d25d14ce37e40686bb6
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-11-02 20:44:03 -0300

    Optimize imports

--- BehavioralPatterns/src/main/java/br/com/douggo/mediator/swing/Client.java
+++ BehavioralPatterns/src/main/java/br/com/douggo/mediator/swing/Client.java
@@ -1,13 +1,5 @@
 package br.com.douggo.mediator.swing;
 
-import br.com.douggo.mediator.swing.components.AddButton;
-import br.com.douggo.mediator.swing.components.Label;
-import br.com.douggo.mediator.swing.components.ResetButton;
-import br.com.douggo.mediator.swing.components.State;
-
-import javax.swing.*;
-import java.awt.*;
-
 public class Client {
 
     public static void main(String[] args) {
--- BehavioralPatterns/src/main/java/br/com/douggo/mediator/swing/components/AddButton.java
+++ BehavioralPatterns/src/main/java/br/com/douggo/mediator/swing/components/AddButton.java
@@ -3,8 +3,6 @@ package br.com.douggo.mediator.swing.components;
 import br.com.douggo.mediator.swing.mediator.Mediator;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class AddButton extends JButton {
 
