6b1f4bae9a0cca28ee38c768027df557e84a8722
Douglas Felipe da Silva <dev.douggo@gmail.com>
2023-11-27 20:16:29 -0300

    Use polymorphism inside the party project and starts another project to implement the observer pattern

--- BehavioralPatterns/src/main/java/br/com/douggo/observer/party/Client.java
+++ BehavioralPatterns/src/main/java/br/com/douggo/observer/party/Client.java
@@ -1,14 +1,15 @@
 package br.com.douggo.observer.party;
 
 import br.com.douggo.observer.party.observers.Friend;
+import br.com.douggo.observer.party.observers.Observer;
 import br.com.douggo.observer.party.observers.Wife;
 import br.com.douggo.observer.party.subjects.Doorman;
 
 public class Client {
 
 	public static void main(String[] args) {
-		Wife wife = new Wife();
-		Friend friend = new Friend();
+		Observer wife = new Wife();
+		Observer friend = new Friend();
 
 		Doorman doorman = new Doorman();
 		doorman.addObserver(wife);
---
+++ BehavioralPatterns/src/main/java/br/com/douggo/observer/swing/Client.java
@@ -0,0 +1,31 @@
+package br.com.douggo.observer.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Client {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();  
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+		JLabel title = new JLabel("Counter");
+		frame.add(title, BorderLayout.NORTH);
+
+		JButton b1 = new JButton("Button #1");
+		JButton b2 = new JButton("Button #2");
+		
+		frame.add(b1, BorderLayout.WEST);
+		frame.add(b2, BorderLayout.EAST);
+		
+		JPanel labelPanel = new JPanel();
+		labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
+		
+		
+		frame.add(labelPanel, BorderLayout.CENTER);
+		
+		frame.setSize(400,200);  
+		frame.setVisible(true);
+	}
+}
